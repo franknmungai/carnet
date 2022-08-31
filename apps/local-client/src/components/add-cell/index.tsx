68b571e93e234b40249fa821f334d04ecb30a46c
franknmungai <frankmnganga@gmail.com>
2022-08-31 22:30:41 +0300

    add cell component

---
+++ apps/local-client/src/components/add-cell/index.tsx
@@ -0,0 +1,29 @@
+const AddCell = () => {
+  return (
+    <div className={`add-cell `}>
+      <div className="add-buttons">
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => {}}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => {}}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
+      <div className="divider"></div>
+    </div>
+  );
+};
+
+export default AddCell;
